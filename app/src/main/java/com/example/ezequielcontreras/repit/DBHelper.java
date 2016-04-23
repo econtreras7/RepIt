934addc5ce3b8f8045861d1a73856ae02d507658
dgistando <davidgistando@gmail.com>
2016-04-23 14:52:06 -0700

    started the sqlite class

--- app/src/main/java/com/example/ezequielcontreras/repit/DBHelper.java
+++ app/src/main/java/com/example/ezequielcontreras/repit/DBHelper.java
@@ -1,7 +1,30 @@
 package com.example.ezequielcontreras.repit;
 
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by dgist on 4/23/2016.
  */
-public class DBHelper {
+public class DBHelper extends SQLiteOpenHelper{
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        
+    }
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public SQLiteDatabase getReadableDatabase() {
+        return super.getReadableDatabase();
+    }
 }
