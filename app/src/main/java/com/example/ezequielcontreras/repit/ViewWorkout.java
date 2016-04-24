18e214b88a9a77ec6587b2775123d360cdb5fb6c
dgistando <davidgistando@gmail.com>
2016-04-24 11:18:32 -0700

    runnable

--- app/src/main/java/com/example/ezequielcontreras/repit/ViewWorkout.java
+++ app/src/main/java/com/example/ezequielcontreras/repit/ViewWorkout.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by dgist on 4/24/2016.
@@ -15,6 +17,8 @@ public class ViewWorkout extends AppCompatActivity {
 
     CardView cardView;
     TextView textView;
+    ImageView image1,image2;
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +26,17 @@ public class ViewWorkout extends AppCompatActivity {
         setContentView(R.layout.view_workout);
         cardView = (CardView)findViewById(R.id.card_view);
         textView =  (TextView)findViewById(R.id.TextView);
+        image1 = (ImageView)findViewById(R.id.image1);
+        image2 = (ImageView)findViewById(R.id.image2);
+        db = new DBHelper(this);
 
+/*        image1.setImageResource(R.drawable.barbell_squats1);
+        image2.setImageResource(R.drawable.barbell_squats2);
+*/
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("", "CardView clicked");
+                Toast.makeText(ViewWorkout.this,"the . . . . test",Toast.LENGTH_LONG).show();
             }
         });
 
