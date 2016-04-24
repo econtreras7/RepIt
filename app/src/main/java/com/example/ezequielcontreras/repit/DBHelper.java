2aef8070b3bbac9cdb5eb0eca9c63d6abde3e574
dgistando <davidgistando@gmail.com>
2016-04-23 23:43:50 -0700

    Put the database called RepIt.db in the assets folder

--- app/src/main/java/com/example/ezequielcontreras/repit/DBHelper.java
+++ app/src/main/java/com/example/ezequielcontreras/repit/DBHelper.java
@@ -2,13 +2,80 @@ package com.example.ezequielcontreras.repit;
 
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by dgist on 4/23/2016.
  */
 public class DBHelper extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME = "RepIt.db";
+    public static final String DATABASE_PATH =
+            "/data/data/com.example.ezequielcontreras.repit/";
+
+    SQLiteDatabase myDatabase;
+    private final Context myContext;
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null,1);
+        this.myContext = context;
+    }
+
+    public void createDataBase() throws IOException {
+        boolean dbExist = checkDatabase();
+        if(!dbExist){
+            this.getReadableDatabase();
+            try{
+                copyDataBase();
+            }catch(IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+
+    private boolean checkDatabase(){
+        SQLiteDatabase checkDB = null;
+        try{
+            String myPath = DATABASE_PATH + DATABASE_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath,null,SQLiteDatabase.OPEN_READONLY);
+        }catch(SQLiteException e){
+            //database dne
+        }
+        if(checkDB != null){
+            checkDB.close();
+        }
+        return checkDB != null ? true :false;
+    }
+
+    private void copyDataBase() throws IOException{
+
+        InputStream myInput = myContext.getAssets().open(DATABASE_NAME);
+        String outFileName = DATABASE_PATH+DATABASE_NAME;
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        byte[] buffer = new byte[1024];
+        int size;
+
+        while((size = myInput.read(buffer))>0){
+            myOutput.write(buffer,0,size);
+        }
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    public void openDatabase() throws SQLException {
+        String myPath = DATABASE_PATH+DATABASE_NAME;
+        myDatabase = SQLiteDatabase.openDatabase(myPath,null,SQLiteDatabase.OPEN_READWRITE);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
@@ -16,15 +83,6 @@ public class DBHelper extends SQLiteOpenHelper{
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        
-    }
 
-    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
-        super(context, name, factory, version, errorHandler);
-    }
-
-    @Override
-    public SQLiteDatabase getReadableDatabase() {
-        return super.getReadableDatabase();
     }
 }
--- app/src/main/java/com/example/ezequielcontreras/repit/MainActivity.java
+++ app/src/main/java/com/example/ezequielcontreras/repit/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.ezequielcontreras.repit;
 
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    DBHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DBHelper(this);
+
+        try{
+            db.createDataBase();
+        }catch (IOException ioe){
+            throw new Error("Can't create databse");
+        }
+
+        try{
+            db.openDatabase();
+        }catch(SQLException sqle){
+            sqle.printStackTrace();
+        }
+
+
     }
 
 }
